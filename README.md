ab87302c5f7a7d40ca3eeb4a7316a7dfdcba8bd4
T.Suzaki <info@aurized-studio.jp>
2017-12-15 04:28:17 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Static Template using Nunjucks + Scss
+When you do Web site coding from nothing.
+
+## Usage
+npm install  
+npm run start
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var plumber = require('gulp-plumber'); // エラーが起きてもwatchを終了しない
+var notify = require('gulp-notify'); // エラーが起こったときの通知
+var nunjucksRender = require('gulp-nunjucks-render');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+// var autoprefixer = require("gulp-autoprefixer"); // 2017/12現在gulp-sourcemapsに対応していないので代わりに↓を使用
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var csscomb = require('gulp-csscomb');
+var replace = require('gulp-replace');
+
+var path = {
+	srcHtml: './src/html/',
+	srcScss: './src/scss/',
+	dest: './htdocs/',
+	destCss: './htdocs/css/'
+};
+
+// エラー通知が必要なタスク使用。通知が必要ない場合には通常のplumberをとるようにする
+// .pipe(plumberWithNotify()) or .pipe(plumber())
+function plumberWithNotify() {
+	return plumber({errorHandler: notify.onError("<%= error.message %>")});
+}
+
+gulp.task('compileHtml', function () {
+	return gulp.src(path.srcHtml + 'page/**/*.html')
+	.pipe(nunjucksRender({
+		path: [path.srcHtml] // String or Array
+	}))
+	.pipe(replace(/\n+/g, '\n')) // 2行以上の改行を1行にする
+	.pipe(gulp.dest(path.dest));
+});
+
+gulp.task('compileScss', function() {
+	return gulp.src(path.srcScss + 'style.scss')
+		.pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
+		.pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(postcss([ autoprefixer({browsers: ['last 2 versions'], grid: true}) ]))
+		// .pipe(replace(/\n+/g, '\n')) // 2行以上の改行を1行にする 納品時の整形に使用
+		// .pipe(csscomb()) // 納品時の整形に使用
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(path.destCss));
+});
+
+gulp.task('serve', ['compileScss', 'compileHtml'], function(){
+	browserSync.init({
+		server: {
+			baseDir: path.dest,
+			directory: true
+		},
+		open: 'external'
+	});
+	gulp.watch(path.srcScss + '**/*.scss', ['compileScss']);
+	gulp.watch(path.srcHtml + '**/*.html', ['compileHtml']);
+	gulp.watch([
+		path.dest + '**/*.html',
+		path.dest + '**/*.css',
+		path.dest + '**/*.js'
+	]).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
